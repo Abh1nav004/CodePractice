762f99136965abe60d56c085f5f34067cde45973
Abh1nav004 <119621660+Abh1nav004@users.noreply.github.com>
2023-10-10 23:50:43 +0530

    add

--- CPP/inheritance.cpp
+++ CPP/inheritance.cpp
@@ -98,4 +98,59 @@
 // }
 
 
+#include<bits/stdc++.h>
+using namespace std;
+class Student{
+    protected:
+    int roll_number;
+    public:
+    void set_roll_number(int);
+    void get_roll_number(void);
+};
+void Student::set_roll_number(int r){
+    roll_number=r;
+}
+void Student::get_roll_number(){
+    cout<<"The roll number is "<<roll_number<<endl;
+}
+class Exam : public Student{
+    protected:
+    float maths;
+    float physics;
+    public:
+    void set_marks(float,float);
+    void get_marks(void);
+};
 
+void Exam :: set_marks(float a, float b){
+    maths = a;
+    physics= b;
+}
+void Exam :: get_marks(){
+   cout<<"Marks of Maths are "<<maths<<endl;
+   cout<<"Marks of Physics are "<<physics<<endl;
+}
+
+class Result : public Exam{
+    float percentage;
+    public:
+    void display(){
+        get_roll_number();
+        get_marks();
+        cout<<"Your percentage is "<<(maths+physics)/2<<"%"<<endl;
+    }
+};
+
+int main(){
+    /*
+    Notes:
+        1. If we are inheriting B from A and C from B:[ A-->B-->C ]
+        2. ABC is called Inheritance Path
+        
+    */
+    Result Abhinav;
+    Abhinav.set_roll_number(420);
+    Abhinav.set_marks(94.0,90.0);
+    Abhinav.display();
+    return 0;
+}
