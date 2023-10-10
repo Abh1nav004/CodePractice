aeee12555299900d47c18c9be70a6dad35952deb
Abh1nav004 <119621660+Abh1nav004@users.noreply.github.com>
2023-10-10 16:47:34 +0530

    changes

---
+++ CPP/abstract.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//abstract class refers to a class containing atleast one pure virtual function,which cannot be instantiated
+
+int main(){
+    
+    return 0;
+}
--- CPP/inheritance.cpp
+++ CPP/inheritance.cpp
@@ -1,5 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+//Base class
+class Employee{
+    int id;
+    float salary;
+};
+
+//Derived class
+// syntax- class {{derived-class-name}}: {{visibilty-mode}} {{base-class-name}}{
+
+// }
+
+//Creating a Programmer class derived from Employee 
 int main(){
     
     return 0;
