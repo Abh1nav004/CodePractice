32431e153e7b8e0c884ce2bf35812e40f69819c3
Abh1nav004 <119621660+Abh1nav004@users.noreply.github.com>
2023-10-11 21:38:22 +0530

    changes

--- CPP/2d_array.cpp
+++ CPP/2d_array.cpp
@@ -73,54 +73,56 @@
 //     return 0;
 // }
 
-#include<bits/stdc++.h>
-using namespace std;
-bool isPresent(int arr[][4],int target, int row,int col){
-    for (int row = 0; row < 3; row++)
-    {
-        for (int col = 0; col < 4; col++)
-        {
-            if(arr[row][col]==target){
-                return true;
-            }
-        }
-        return false;
-    }
+// #include<bits/stdc++.h>
+// using namespace std;
+// bool isPresent(int arr[][4],int target, int row,int col){
+//     for (int row = 0; row < 3; row++)
+//     {
+//         for (int col = 0; col < 4; col++)
+//         {
+//             if(arr[row][col]==target){
+//                 return true;
+//             }
+//         }
+//         return false;
+//     }
     
-}
-void printSum(int arr[][4],int row,int col){
-    int sum=0;
-    for (int row = 0; row < 3; row++)
-    {
-        for (int col = 0; col < 4; col++)
-        {
-           sum=sum+arr[row][col];
-        }
+// }
+// void printSum(int arr[][4],int row,int col){
+//     int sum=0;
+//     for (int row = 0; row < 3; row++)
+//     {
+//         for (int col = 0; col < 4; col++)
+//         {
+//            sum=sum+arr[row][col];
+//         }
         
-    }
+//     }
 
-    cout<<"Sum is "<<sum;
+//     cout<<"Sum is "<<sum;
     
-}
-int main(){
-    int arr[3][4]={1,2,3,4,5,6,7,8,9,10,11,12};
+// }
+// int main(){
+//     int arr[3][4]={1,2,3,4,5,6,7,8,9,10,11,12};
 
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            cout<<arr[i][j]<<" ";
-        } cout<<endl;
+//     for (int i = 0; i < 3; i++)
+//     {
+//         for (int j = 0; j < 4; j++)
+//         {
+//             cout<<arr[i][j]<<" ";
+//         } cout<<endl;
         
-    }
+//     }
 
-    cout<<"Element to search: ";
-    int target;
-    cin>>target;
+//     cout<<"Element to search: ";
+//     int target;
+//     cin>>target;
 
-    isPresent(arr,target,3,4);
-    printSum(arr,3,4);
+//     isPresent(arr,target,3,4);
+//     printSum(arr,3,4);
 
     
-    return 0;
-}
+//     return 0;
+// }
+
+// smthng written for pushing code in git;
