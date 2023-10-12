a6c1b76648df596960805ea953715a085eba3d2f
Abh1nav004 <119621660+Abh1nav004@users.noreply.github.com>
2023-10-12 20:13:56 +0530

    changes6

--- DSA/tree.cpp
+++ DSA/tree.cpp
@@ -1,3 +1,32 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// class Node{
+//     public:
+//     int data;
+//     Node *left;
+//     Node *right;
+
+//     Node(int val){
+//         data = val;
+//         left=NULL;
+//         right=NULL;
+//     }
+// };
+// int main(){
+
+//     Node* root= new Node(1);
+//     root->left = new Node(2);
+//     root->right = new Node(3);
+
+//     root->left->left = new Node(4);
+//     root->left->right = new Node(5);
+//     root->right->left = new Node(6);
+//     root->right->right = new Node(7);
+    
+//     return 0;
+// }
+
+
 #include<bits/stdc++.h>
 using namespace std;
 class Node{
@@ -12,16 +41,65 @@ class Node{
         right=NULL;
     }
 };
-int main(){
 
-    Node* root= new Node(1);
-    root->left = new Node(2);
-    root->right = new Node(3);
 
-    root->left->left = new Node(4);
-    root->left->right = new Node(5);
-    root->right->left = new Node(6);
-    root->right->right = new Node(7);
+Node* insertBST(Node* root,int val){
+    if(root==NULL){
+        return new Node(val);
+    }
+    if(val<root->data){
+        root->left = insertBST(root->left,val);
+    }
+    else
+    {
+        root->right =insertBST(root->right,val);
+    }
+    return root;
     
-    return 0;
+}
+
+void printLevelOrder(Node* root){
+    if(root == NULL) return;
+    queue<Node*>q;
+
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty()){
+        Node* node=q.front();
+        q.pop();
+
+        if(node!=NULL){
+            cout<<node->data<<" ";
+            if (node->left)
+            {
+                q.push(node->left);
+            }
+            if (node->right)
+            {
+                q.push(node->right);
+            }
+        }
+        
+       else if (!q.empty())
+        {
+            q.push(NULL);
+        }
+        
+    }
+
+
+    
+}
+int main(){
+    Node* root=new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+    
+    root->left->left=new Node(4);
+    root->left->right=new Node(5);
+    root->right->left=new Node(6);
+    root->right->right=new Node(7);
+
+    printLevelOrder(root);
 }
