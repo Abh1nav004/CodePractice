236b9cd58e73814c6aacb8314b4276a9958d577c
Abh1nav004 <119621660+Abh1nav004@users.noreply.github.com>
2023-10-16 21:35:53 +0530

    abstract

--- CPP/abstract.cpp
+++ CPP/abstract.cpp
@@ -2,8 +2,62 @@
 using namespace std;
 
 //abstract class refers to a class containing atleast one pure virtual function,which cannot be instantiated
+// class Shape{
+    // public:
+    // int shape_width;
+    // int shape_height;
 
+    // void width(int w){
+    //     shape_width=w;
+    // }
+    // void height(int h){
+    //     shape_height=h;
+    // }
+//     int perimeterOfSquare(int side){
+//         return 4*side;
+//     }
+//     int PerimeterOfRectangle(int l,int b){
+//         return 2* (l+b);
+//     }
+//     //All the functions of square and rectangle are clubbed in a single class
+// };
+
+class Shape{
+    public:
+    int shape_width;
+    int shape_height;
+
+    void width(int w){
+        shape_width=w;
+    }
+    void height(int h){
+        shape_height=h;
+    }
+    virtual int perimeter()=0;
+};
+
+class Rectangle : public Shape{
+    public:
+    int perimeter(){
+        return 2*(shape_height+shape_width);
+    }
+};
+
+class Square : public Shape{
+    public:
+    int perimeter(){
+        return 4*shape_width;
+    }
+};
 int main(){
+    Rectangle r1;
+    Square s1;
+    
+    r1.width(10);
+    r1.height(5);
+    s1.width(12);
+
+    cout<<r1.perimeter();
     
     return 0;
 }
