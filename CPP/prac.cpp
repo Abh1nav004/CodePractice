373e1e9f3a9ec8760df292990b56b0395dd30c33
Abh1nav004 <119621660+Abh1nav004@users.noreply.github.com>
2023-10-22 15:59:53 +0530

    changes

--- CPP/prac.cpp
+++ CPP/prac.cpp
@@ -485,3 +485,71 @@
 //     cout << result;
 //     return 0;
 // }
+
+// #include <iostream>
+// using namespace std;
+
+// class Distance {
+// public:
+//     int meters;
+//     Distance(int m) : meters(m) {}
+//     operator int() {
+//         return meters + 78;
+//     }
+// };
+
+// int main() {
+//     Distance d(500);
+//     int result = d;
+//     cout << result;
+//     return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// class Angle {
+// public:
+//     float degrees;
+//     Angle(float d) : degrees(d) {}
+//     operator float() {
+//         return degrees-8.9;
+//     }
+// };
+
+// int main() {
+//     Angle a(45.5);
+//     int result = a;
+//     cout << result;
+//     return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+// class Base
+// {
+// public:
+//     Base() {}
+//     ~Base() {}
+
+// protected:
+// private:
+// };
+
+// class Derived : public Base
+// {
+// public:
+//     Derived() {}
+//     ~Derived() {}
+
+// private:
+// protected:
+// };
+
+// int main()
+// {
+//     cout << "The program executed" << endl;
+//     return 0;
+// }
