c0c782ccbc3f8003c9ba6a643aa22f251fb53226
Abh1nav004 <119621660+Abh1nav004@users.noreply.github.com>
2023-10-29 00:34:08 +0530

    hh

--- DSA/arrays/selection_sort.cpp
+++ DSA/arrays/selection_sort.cpp
@@ -1,6 +1,29 @@
+//used when array size is small
+
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
+void selectionSort(vector<int>&arr,int n){
     
+    for (int i = 0; i < n-1; i++)
+    {
+        int minIndex=i;
+        for (int j = i+1; j < n; j++)
+        {
+           if (arr[j]<arr[minIndex])
+            minIndex=j;
+        }
+        swap(arr[minIndex],arr[i]);
+        
+    }
+    
+}
+int main(){
+    vector<int>a={6,3,9,2,1,0};
+
+    selectionSort(a,6);
+    for (int i = 0; i < 6; i++)
+    {
+        cout<<a[i]<<" ";
+    }   
     return 0;
 }
