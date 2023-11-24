1839c26023b32afa22673c571f22c7e0c80ffb6b
Abh1nav004 <119621660+Abh1nav004@users.noreply.github.com>
2023-11-24 15:06:12 +0530

    changes

--- CPP/File_io.cpp
+++ CPP/File_io.cpp
@@ -6,22 +6,23 @@
 // ifstream
 // ofstream
 
-using namespace std;
-int main(){
-//     // string st="Abhinav bhai ke aage koi bol sakta hai kyaaaaaaa Aeyyyyyyy abhinav bhaiiiiiiiiiiii";
-    string st2;
-//     // //Opening files using constructor and writing it
-//     // ofstream out("sample.txt"); //write operation
-//     // out<<st;
+// using namespace std;
+// int main(){
+// //     // string st="Abhinav bhai ke aage koi bol sakta hai kyaaaaaaa Aeyyyyyyy abhinav bhaiiiiiiiiiiii";
+//     string st2;
+// //     // //Opening files using constructor and writing it
+// //     // ofstream out("sample.txt"); //write operation
+// //     // out<<st;
 
 
-    //Opening files using constructor and reading it
-    ifstream in("sample1.txt"); //Read operation
-    // in>>st2;
-    getline(in,st2);
-    getline(in,st2);
-    cout<<st2;
-    return 0;
-}
+//     //Opening files using constructor and reading it
+//     ifstream in("sample1.txt"); //Read operation
+//     // in>>st2;
+//     getline(in,st2);
+//     getline(in,st2);
+//     cout<<st2;
+//     return 0;
+// }
 
 
+ 
